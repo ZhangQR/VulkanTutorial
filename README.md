dd71fc9d2fd1669281cf5b9159d8d4abfb234019
ZhangQR <ricey54560@gmail.com>
2022-11-01 00:47:05 +0800

    Setup->Base code

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ cmake --build .
 ```
 
 vscode 直接 F5，或者 Ctrl+Shift+P，run task，cmake。  
+
+## TODO
+
+Resource management: RAII
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,69 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <cstdlib>
 
-int main() {
+const static int WIDTH = 800;
+const static int HEIGHT = 600;
+
+class VulkanApp
+{
+public:
+    void run();
+
+private:
+    GLFWwindow *window;
+    void initGlfw();
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+};
+
+void VulkanApp::run()
+{
+    initGlfw();
+    initVulkan();
+    mainLoop();
+    cleanup();
+}
+
+void VulkanApp::initGlfw()
+{
     glfwInit();
-
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "My Vulkan Window", NULL, NULL);
+}
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+void VulkanApp::initVulkan()
+{
+}
 
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
+void VulkanApp::mainLoop()
+{
+    while (!glfwWindowShouldClose(window))
+    {
         glfwPollEvents();
     }
+}
 
+void VulkanApp::cleanup()
+{
     glfwDestroyWindow(window);
-
     glfwTerminate();
+}
 
-    return 0;
+int main(int argc, const char **argvs)
+{
+    VulkanApp app;
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
